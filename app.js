17b7ccca0ad999351645ca624e87326600222267
D1ego <diegommor@gmail.com>
2026-02-24 12:32:20 -0600

    Body parser

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
