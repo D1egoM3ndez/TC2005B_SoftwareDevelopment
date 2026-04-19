46dccee5b5fac89446c4b1e2e7fecb8e75c2d737
D1ego <diegommor@gmail.com>
2026-04-18 20:45:00 -0600

    Termino de Laboratorio 23

--- models/user.model.js
+++ models/user.model.js
@@ -5,23 +5,22 @@ module.exports = class User {
 
     constructor(mi_username, mi_nombre, mi_password, mi_correo) {
         this.username = mi_username;
-        this.nombre = mi_nombre;
+        this.nombre   = mi_nombre;
         this.password = mi_password;
-        this.correo = mi_correo;
+        this.correo   = mi_correo;
     }
 
     save() {
         return bcrypt.hash(this.password, 12).then((password_cifrado) => {
             return db.execute(
-                "INSERT INTO usuarios(username, nombre, password, correo) VALUES (?, ?, ?, ?)" ,
+                "CALL sp_guardar_usuario(?, ?, ?, ?)",
                 [this.username, this.nombre, password_cifrado, this.correo]
             );
         }).catch((error) => {
             console.log(error);
-            next(error);
         });
     }
-    
+
     static fetchOne(username) {
         return db.execute("SELECT * FROM usuarios WHERE username = ?", [username]);
     }
@@ -29,8 +28,7 @@ module.exports = class User {
     static getPermisos(username) {
         return db.execute(
             `SELECT nombre_privilegio FROM tiene t, roles r, posee p, privilegios pr
-            WHERE id_usuario = ? AND t.id_rol=r.id AND r.id=p.id_rol AND p.id_privilegio=pr.id`, 
+            WHERE id_usuario = ? AND t.id_rol=r.id AND r.id=p.id_rol AND p.id_privilegio=pr.id`,
             [username]);
     }
-
 }
